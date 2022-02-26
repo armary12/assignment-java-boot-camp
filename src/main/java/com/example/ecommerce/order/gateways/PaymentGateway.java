4f01efbfd7ab1f831e0952baf1ac2d733c92ba40
Chonlatid Jitbute <chonlatid.jit@gmail.com>
2022-02-26 18:23:18 +0700

    feat: add paymentGatewayEndpoint config #12

--- src/main/java/com/example/ecommerce/order/gateways/PaymentGateway.java
+++ src/main/java/com/example/ecommerce/order/gateways/PaymentGateway.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.order.gateways;
 import com.example.ecommerce.order.models.PaymentGatewayRequest;
 import com.example.ecommerce.order.models.PaymentGatewayResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +13,15 @@ public class PaymentGateway {
     @Autowired
     private RestTemplate restTemplate;
 
+    private String paymentGateWayEndpoint;
 
-    public PaymentGatewayResponse submitPayment(PaymentGatewayRequest request){
-        return null;
+    public PaymentGateway(@Value("${PAYMENT_GATEWAY_ENDPOINT}") String paymentGateWayEndpoint) {
+        this.paymentGateWayEndpoint = paymentGateWayEndpoint;
+    }
+
+    public PaymentGatewayResponse submitPayment(PaymentGatewayRequest request) {
+
+        PaymentGatewayResponse paymentGatewayResponse = restTemplate.postForObject(paymentGateWayEndpoint, request, PaymentGatewayResponse.class);
+        return paymentGatewayResponse;
     }
 }
